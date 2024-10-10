b2ae7392fd7205c7e5e6bb618c3abd4c159f3f46
gityashh <yashrajput8359@gmail.com>
2024-10-10 23:39:38 +0530

    puppeteer configured

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes/index-router");
+const puppeteerConfig = require("./config/puppeteer.config");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", indexRouter);
+app.get("/", (res, req) => {
+    res.send("Hello World");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- backend/config/puppeteer.config.js
+++ backend/config/puppeteer.config.js
@@ -1,1 +1,29 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
+
+async function scrapeTest() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--no-zygote',
+      '--single-process',
+    ]
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://leetcode.com/problems/two-sum/description/', { waitUntil: 'domcontentloaded' });
+
+//   const heading = await page.evaluate(() => {
+//     return document.querySelector('h1').innerText;
+    //   });
+    
+    const content = await page.content();
+
+  ;
+  return content;
+}
+
+scrapeTest().then(console.log).catch(console.error);
